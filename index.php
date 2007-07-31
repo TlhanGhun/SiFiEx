b48c08ee01145b1f4dbbee0be921db462fdb68ce
Tlhan Ghun <info@tlhan-ghun.de>
2007-07-31 10:43:55 +0000

    Updated the error message for not being able to create config.php to be more visible (red area now)

--- index.php
+++ index.php
@@ -3,7 +3,8 @@
     if (copy("config.php.templ", "config.php")) {
       $firstStart = TRUE;
     } else {
-      echo "<strong>Sorry, I was unable to create the default configuration for you. Please copy manually the file called config.php.templ to config.php in the folder where you installed SiFiEx.</strong>. It looks like I didn't have enough rights to write this file for you...";
+      echo "<br /><br /><br /><div style=\"background-color:red;border:1px solid black;padding:1em;\"><strong>Sorry, I was unable to create the default configuration for you. Please copy manually the file called config.php.templ to config.php in the folder where you installed SiFiEx.</strong>. It looks like I didn't have enough rights to write this file for you...<br /><br />SiFiEx</div>";
+     die();
     }
   }
   require_once("functions.php");
