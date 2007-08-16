c85cc05a15136037f8a1aa1eb3e72dc38969edba
Tlhan Ghun <info@tlhan-ghun.de>
2007-08-16 12:11:27 +0000

    setup should now be finished

--- setup/setup.php
+++ setup/setup.php
@@ -9,23 +9,25 @@ class setup {
     echo "<div id=\"setup\">\n";
     echo "<h1>Installation</h1>\n";
     echo "<p>Welcome to the firsttime setup of SiFiEx on this machine. To be able to share files SiFiEx will need some writing rights on your webspace. I will check this for you right now...</p>\n";
-    echo "  <p>Checking if I'm allowed to write the initial configuration: ";
+    echo "<ul>\n";
+    echo "  <li>Checking if I'm allowed to write the initial configuration or if it is already there: ";
     if ($this->checkWritableConfig ()) {
       echo "<span class=\"OK\">OK</span>";
     } else {
       echo "<span class=\"NotOK\">NOT OK</span>";
       $this->configProblem = TRUE;
     }
-    echo "</p>\n";
+    echo "  </li>\n";
 
-    echo "  <p>Checking if I'm allowed to write files into the exchange directory: ";
+    echo "  <li>Checking if I'm allowed to write files into the exchange directory: ";
     if ($this->checkWritableFilesFolder ()) {
       echo "<span class=\"OK\">OK</span>";
     } else {
       echo "<span class=\"NotOK\">NOT OK</span>";
       $this->filesProblem = TRUE;
     }
-    echo "</p>\n";
+    echo "  </li>\n";
+    echo "</ul>\n";
     
     if(!$this->foundProblems()) {
       echo "<p>Everything seems to be fine on your computer. I now will create a default configuration for you in the file called &quot;config.php&quot;. Maybe you want to change some settings in this file using a simple texteditor to adapt SiFiEx to your needs.</p>\n";
@@ -35,7 +37,7 @@ class setup {
       echo "</body></html>";
       die();
    } else {
-      echo "<p>Sorry, I don't have enough rights to finish the installation for you. Please read the following paragraphs to change the permissions.</p>\n";
+      echo "<p>Sorry, I don't have enough rights to finish the installation for you. Please read the following paragraphs to finish the installation.</p>\n";
       echo "</div>\n";
       $this->fixProblems($this->configProblem, $this->filesProblem);
     }
@@ -52,6 +54,20 @@ class setup {
   function fixProblems($files, $config) {
     echo "<div id=\"describeSetupProblems\">\n";
     echo "<h1>Howto make needed adjustments</h1>\n";
+    if ($config) {
+      echo "<h2>Install config.php</h2>\n";
+      echo "<p>You have now two options to install the default configuration - just choose the one you prefer...</p>\n";
+      echo "<h3>Creating config.php by yourself</h3>\n";
+      echo "<p>SiFiEx tried to install a default configuration file for you. To reach this goal it copies the included file &quot;config.php.templ&quot; to a file called &config.php&quot;. The easiest way to solve this is simply that you just copy it yourself in this way and upload the &quot;config.php&quot; to your webserver in the installation directory.</p>\n";
+      echo "<h3>Change permissions for automatic installation</h3>\n";
+      echo "<p>The other way is to change the write permissions on the topmost folder of your installation (normally &quot;/SiFiEx&quot;) as shown in the examples at the bottom of this page.</p>\n";
+      echo "<p>Now reload the start page of SiFiEx the the configuration will be created for you</p>\n";
+    }
+    if ($files) {
+      echo "<h2>Changing permissions for the files-folder</h2>\n";
+      echo "<p>To be able to upload files to the webserver SiFiEx needs write permissions in the files folder. See below some example howto make this change.</p>\n";
+      echo "<p>Background: On the webservers of mass hosters normally the user uploading files via FTP (the way you most probably installed SiFiEx) and the user running the PHP-scripts are not equal. So most of the times on such servers the PHP-script is not allowed to upload files to the files directory until you change those permissions to make this folder writable for the PHP-user.</p>\n";
+    }
     echo "</div>\n";
     echo "<div id=\"fixSetupProblems\">\n";
     readfile("setup/examplesFTP.html");
@@ -62,10 +78,15 @@ class setup {
 
   function checkWritableConfig () {
     # return FALSE;
-    return(is_writable("./"));
+    if (is_writable("./") || file_exists("config.php")) {
+      return TRUE;
+    } else {
+      return FALSE;
+    }
   }
 
   function checkWritableFilesFolder () {
+    # return FALSE;
     return(is_writable("./files/"));
   }
 
