1c8e56e69e582070f7f87bcc2f2e12e104123903
Tlhan Ghun <info@tlhan-ghun.de>
2007-08-22 11:59:54 +0000

    "Beginning some cleanup "

--- functions.php
+++ functions.php
@@ -1,101 +1,108 @@
 <?php
 
 function displayFilename($filename, $length) {
-  if(($length < strlen($filename)) && $length) {
-    $leftPart = substr($filename, 0, intval($length/2-1));
-    $rightPart = substr($filename, intval(strlen($filename) - ($length/2 -1)));
-    return $leftPart."..".$rightPart;
-  } else {
-    return $filename;
-  }
+	if(($length < strlen($filename)) && $length) {
+		$leftPart = substr($filename, 0, intval($length/2-1));
+		$rightPart = substr($filename, intval(strlen($filename) - ($length/2 -1)));
+		return $leftPart."..".$rightPart;
+	} else {
+		return $filename;
+	}
 }
 
 function getMaximumUploadSize () {
-  return size_hum_read(min(sizeCpuReadable(ini_get('post_max_size')),sizeCpuReadable(ini_get('upload_max_filesize')))); 
+	return size_hum_read(min(sizeCpuReadable(ini_get('post_max_size')),sizeCpuReadable(ini_get('upload_max_filesize'))));
 }
 
 function writeWarning($warnText) {
-  echo "<div id=\"warning\">\n";
-  echo "  <p>$warnText</p>\n";
-  echo "</div>\n";  
+	echo "<div id=\"warning\">\n";
+	echo "  <p>$warnText</p>\n";
+	echo "</div>\n";
 }
 function writeOngoing($text) {
-  global $conf;
-  if($conf['showOngoing']) {
-    echo "<div id=\"ongoing\">\n";
-    echo "  <p>$text</p>\n";
-    echo "</div>\n";  
-  }
+	global $conf;
+	if($conf['showOngoing']) {
+		echo "<div id=\"ongoing\">\n";
+		echo "  <p>$text</p>\n";
+		echo "</div>\n";
+	}
 }
 function writeSuccess($text) {
-  echo "<div id=\"success\">\n";
-  echo "  <p>$text</p>\n";
-  echo "</div>\n";  
+	echo "<div id=\"success\">\n";
+	echo "  <p>$text</p>\n";
+	echo "</div>\n";
 }
 function sendMail($receipient, $fileName, $conf, $lang) {
-  $header = "";
-  $header .= "From: ".$conf['mailSenderName']." <".$conf['mailSenderEmail'].">\r\n"; 
-  $body = "";
-  $body .= $lang['mailStart']." "; 
-  $pathFull = explode("/", $_SERVER['PHP_SELF']);
-  array_pop($pathFull);
-  $pathToScript=implode("/", $pathFull);
-  $body .= detectSSL()."://".$_SERVER['HTTP_HOST'].$pathToScript."/files/".$fileName."\n\n"; 
-  if ($conf['mailInfoPassword']) {
-    $body .= $lang['mailPassword']."\n\n"; 
-  }
-  $body .= "\n\n".$lang['mailEnd']; 
-  if (!mail($receipient, $lang['mailSubject'], $body, $header)) {
-    writeWarning($lang['mailError']);
-  } else {
-    writeSuccess($lang['mailSuccess'].$receipient);
-  }
+	$header = "";
+	$header .= "From: ".$conf['mailSenderName']." <".$conf['mailSenderEmail'].">\r\n";
+	$body = "";
+	$body .= $lang['mailStart']." ";
+	$pathFull = explode("/", $_SERVER['PHP_SELF']);
+	array_pop($pathFull);
+	$pathToScript=implode("/", $pathFull);
+	$body .= detectSSL()."://".$_SERVER['HTTP_HOST'].$pathToScript."/files/".$fileName."\n\n";
+	if ($conf['mailInfoPassword']) {
+		$body .= $lang['mailPassword']."\n\n";
+	}
+	$body .= "\n\n".$lang['mailEnd'];
+	if (!mail($receipient, $lang['mailSubject'], $body, $header)) {
+		writeWarning($lang['mailError']);
+	} else {
+		writeSuccess($lang['mailSuccess'].$receipient);
+	}
 
-#   echo "<pre>".$header."\n\n".$body."</pre>\n";
+	#   echo "<pre>".$header."\n\n".$body."</pre>\n";
 }
 
 function size_hum_read($size){
-  $i=0;
-  $iec = array("Byte", "KByte", "MByte", "GByte", "TByte", "PByte", "EByte", "ZByte", "YByte");
-  while (($size/1024)>1) {
-    $size=$size/1024;
-	  $i++;
+	$i=0;
+	$iec = array("Byte", "KByte", "MByte", "GByte", "TByte", "PByte", "EByte", "ZByte", "YByte");
+	while (($size/1024)>1) {
+		$size=$size/1024;
+		$i++;
 	}
-  return substr($size,0,strpos($size,'.')+4)." ".$iec[$i];
+	return substr($size,0,strpos($size,'.')+4)." ".$iec[$i];
 }
 
 function sizeCpuReadable($size) {
-  if (preg_match('/K$/', $size)) {
-    return $size * 1024;
-  } elseif (preg_match('/M$/', $size)) {
-    return $size * 1024 * 1024;
-  } elseif (preg_match('/G$/', $size)) {
-    return $size * 1024 * 1024 * 1024;
-  } else {
-    return $size;
-  }
+	if (preg_match('/K$/', $size)) {
+		return $size * 1024;
+	} elseif (preg_match('/M$/', $size)) {
+		return $size * 1024 * 1024;
+	} elseif (preg_match('/G$/', $size)) {
+		return $size * 1024 * 1024 * 1024;
+	} else {
+		return $size;
+	}
 }
 
 function detectSSL(){
-  if($_SERVER["https"] == "on"){
-    return "https";
-  } elseif ($_SERVER["https"] == 1){
-    return "https";
-  } elseif ($_SERVER['SERVER_PORT'] == 443) {
-    return "https";
-  } else {
-    return "http";
-  }
-} 
+	if($_SERVER["https"] == "on"){
+		return "https";
+	} elseif ($_SERVER["https"] == 1){
+		return "https";
+	} elseif ($_SERVER['SERVER_PORT'] == 443) {
+		return "https";
+	} else {
+		return "http";
+	}
+}
 
-	
+function showResult ($value, $goodText, $badText) {
+	if ($value) {
+		echo "<span class=\"OK\">".$goodText."</span>";
+	} else {
+		echo "<span class=\"NotOK\">".$badText."</span>";
+	}
+}
+
 
 if (!function_exists('ftp_chmod')) {
-  function ftp__chmod($ftpstream,$chmod,$file) {
-    $old=error_reporting();
-    #error_reporting(0);
-    $result=ftp_site($ftpstream, "CHMOD ".$chmod." ".$file);
-    #error_reporting($old);
-    return $result;
-  }
+	function ftp_chmod($ftpstream,$chmod,$file) {
+		$old=error_reporting();
+		#error_reporting(0);
+		$result=ftp_site($ftpstream, "CHMOD ".$chmod." ".$file);
+		#error_reporting($old);
+		return $result;
+	}
 }
