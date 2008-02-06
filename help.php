3ae4e3fcb0e214e09f9859aab6d519e77779e34f
Tlhan Ghun <info@tlhan-ghun.de>
2008-02-06 10:54:57 +0000

    0.4.1 release added forgotten help.php added new theme "dark"

---
+++ help.php
@@ -0,0 +1,33 @@
+<?php
+session_start();
+require_once("functions.php");
+require_once("config.php");
+
+if ($_SESSION['language']) {
+  $config['language'] =$_SESSION['language']; 
+}
+if ($_SESSION['theme']) {
+  $config['theme'] =$_SESSION['theme']; 
+}
+require_once("languageFiles/".$config['language']."/texts.php");
+?>
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN"
+    "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="de" lang="de">
+  <head>
+    <meta http-equiv="content-type" content="text/html; charset=iso-8859-1" />
+    <title>Help - SiFiEx - Simple File Exchange <?php echo $config['version']; ?></title>
+    <meta name="generator" content="Sven Walther" />
+    <link rel="Shortcut Icon" type="image/x-icon" href="favicon.ico" />
+    <link href="<?php echo "themes/".$config['theme']."/stylesheet.css"; ?>" rel="stylesheet" type="text/css" />
+  </head>
+  <body>
+      <div id="logoHeader">
+      <h1><a href="index.php">Simple File Exchange <?php echo $config['version']; ?></a></h1>
+    </div>
+    <div id="faq">
+      <h2>FAQ</h2>
+      <?php readfile("languageFiles/".$config['language']."/faq.html") ?>
+    </div>
+  </body>
+</html>
